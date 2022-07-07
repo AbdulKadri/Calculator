9763b5a7c06555cbf5e79b6b230c427697959c1c
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2022-07-06 20:33:13 -0600

    Introduce operate function

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ console.log(add(4, 2))
 console.log(subtract(4, 2))
 console.log(multiply(4, 2))
 console.log(divide(4, 2))
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2)
+        case '-':
+            return subtract(num1, num2)
+        case '*':
+            return multiply(num1, num2)
+        case '/':
+            return divide(num1, num2)
+    }
+}
