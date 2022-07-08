ca8cd4a17ae1ec4852d61872a199fe17b2aa42d7
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2022-07-07 20:51:25 -0600

    Add button functions

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+let initialNumber = ''
+let secondNumber = ''
+let initialOperator = null
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorButtons = document.querySelectorAll('[data-operator]')
+const btns = document.querySelectorAll('.btn')
+const clear = document.getElementById('clear')
+const pos_neg = document.getElementById('pos_neg')
+const equal = document.getElementById('equal')
+const screen = document.getElementById('screen')
+const results = document.getElementById('results')
+
+numberButtons.forEach((number) => {
+    number.addEventListener('click', () => changeScreen(number.textContent))
+    // initialNumber = number.textContent
+    // results.textContent = initialNumber
+})
+
+operatorButtons.forEach((operator) => {
+    operator.addEventListener('click', () => changeScreen(operator.textContent))
+    // initialOperator = operator.textContent
+    // console.log(initialOperator)
+})
+
+clear.addEventListener('click', clearScreen)
+
+function changeScreen(value) {
+    screen.textContent += value
+}
+
+function clearScreen() {
+    screen.textContent = ''
+}
+
 function add(num1, num2) {
     return num1 + num2
 }
@@ -13,10 +48,6 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2
 }
-console.log(add(4, 2))
-console.log(subtract(4, 2))
-console.log(multiply(4, 2))
-console.log(divide(4, 2))
 
 function operate(operator, num1, num2) {
     switch (operator) {
@@ -28,5 +59,7 @@ function operate(operator, num1, num2) {
             return multiply(num1, num2)
         case '/':
             return divide(num1, num2)
+        default:
+            return null
     }
 }
